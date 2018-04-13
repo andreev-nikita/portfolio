f4f8af7ee46137a697b5f1b862e48ba84f5d6134
Nikita Andreev <andreev_nikita@list.ru>
2018-04-13 19:41:36 +0300

    Scroll section in portfolio was added

---
+++ source/js/divslider.js
@@ -0,0 +1,49 @@
+$(document).ready( () => {
+
+	let width = $('.divslider').width(),
+		itemsCount = $('.divslider__viewport').children().length,
+		currentItem = 1;
+
+	let setViewportWidth = () => {
+		width = $('.divslider').width();
+		let viewportWidth = (itemsCount) * width;
+		let currentTransform = -((currentItem - 1) * width);
+		$('.divslider__viewport').css('transition', 'all 0s ease 0s');
+		$('.divslider__viewport').css('transform', `translateX(${currentTransform}px)`);
+		$('.divslider__viewport').css('width', `${viewportWidth}px`);
+		$('.divslider__item').css('width', width);
+		$('.divslider__viewport').css('visibility', 'visible');
+		$('.divslider__viewport').css('transition', 'transform 1s ease 0s');
+	}
+
+	let slideToTheNext = () => {
+		if(currentItem < itemsCount) {
+			let currentTransform = -(currentItem * width);
+			$('.divslider__viewport').css('transform', `translateX(${currentTransform}px)`);
+			$(`.slider-selectors__item:nth-child(${currentItem})`).removeClass('slider-selectors__item_active');
+			currentItem++;
+			$(`.slider-selectors__item:nth-child(${currentItem})`).addClass('slider-selectors__item_active');
+		}
+	}
+
+	let slideToThePrev = () => {
+		if(currentItem > 1) {
+			let currentTransform = -((currentItem - 2)* width);
+			$('.divslider__viewport').css('transform', `translateX(${currentTransform}px)`);
+			$(`.slider-selectors__item:nth-child(${currentItem})`).removeClass('slider-selectors__item_active');
+			currentItem--;
+			$(`.slider-selectors__item:nth-child(${currentItem})`).addClass('slider-selectors__item_active');
+		}
+	}
+
+	setViewportWidth();
+	$(window).resize(setViewportWidth);
+
+	$('#next-slide').click( () => {
+		slideToTheNext();
+	})
+
+	$('#prev-slide').click( () => {
+		slideToThePrev();
+	})
+})
