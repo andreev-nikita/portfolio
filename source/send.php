11dbaced0eb45b933957765d8667ae745f4a80e0
Nikita Andreev <andreev_nikita@list.ru>
2018-05-10 20:17:03 +0300

    Mail address and some text in the index.html file were fixed

--- source/send.php
+++ source/send.php
@@ -1,8 +1,8 @@
 <?php
-	if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['mail'])&&$_POST['mail']!="")&&(isset($_POST['text'])&&$_POST['text']!="")){ 
-	    $to = 'nikitaandreev@gmail.com'; 
-	    $subject = 'Message from site'; 
-	    $message = '
+    if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['mail'])&&$_POST['mail']!="")&&(isset($_POST['text'])&&$_POST['text']!="")){ 
+        $to = 'nikitaandreevcb@gmail.com'; 
+        $subject = 'Message from site'; 
+        $message = '
             <html>
                 <head>
                     <title>'.$subject.'</title>
@@ -13,8 +13,8 @@
                     <p>Сообщение:<br> '.$_POST['text'].'</p>              
                 </body>
             </html>'; 
-	    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-	    $headers .= "From: Отправитель <admin@nikita-andreev.ru>\r\n"; 
-	    mail($to, $subject, $message, $headers);
-	}
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "From: Отправитель <admin@nikita-andreev.ru>\r\n"; 
+        mail($to, $subject, $message, $headers);
+    }
 ?>
